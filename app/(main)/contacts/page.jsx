66be4f903e00ab1850d4f7f792c11305f864a25a
Dhruvi Bhuva <dhruvibhuva1719@gmail.com>
2026-05-21 19:35:44 +0530

    fix

--- app/(main)/contacts/page.jsx
+++ app/(main)/contacts/page.jsx
@@ -3,14 +3,15 @@
 import { useState, useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Link from "next/link";
-import { api } from "@/convex/_generated/api";
-import { useConvexQuery } from "@/hooks/use-convex-query";
+import { api } from "../../../convex/_generated/api";
+import { useConvexQuery } from "../../../hooks/use-convex-query";
 import { BarLoader } from "react-spinners";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "../../../components/ui/button";
+import { Card, CardContent } from "../../../components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "../../../components/ui/avatar";
 import { Plus, Users, User } from "lucide-react";
 import { CreateGroupModal } from "./components/create-group-modal";
+import { Input } from "../../../components/ui/input"
 
 export default function ContactsPage() {
   const [isCreateGroupModalOpen, setIsCreateGroupModalOpen] = useState(false);
